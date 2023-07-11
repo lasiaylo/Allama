c03938088cf7df52fb6e90b77ca5423225c79e92
lasia <lasiaylo@gmail.com>
2023-07-11 15:18:08 -0400

    Remove year from sidebar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,4 +1,4 @@
-import FunctionMenu, { IButton } from './util/functionMenu';
+import FunctionMenu from './util/functionMenu';
 import * as React from 'react';
 import { IWork } from '../util/page/IndexUtils';
 import '../styles/components/s_sidebar.scss';
@@ -9,46 +9,18 @@ type PropTypes = {
   activeWork: IWork;
 };
 
-export default function Sidebar({
-  works,
-  callback,
-  activeWork,
-}: PropTypes) {
-  const yearToWork: Record<string, IWork[]> = {};
-  // TODO: Convert to reduce
-  works.forEach((work) => {
-    const year = new Date(work.datePublished).getFullYear().toString();
-    if (yearToWork[year] === undefined) {
-      yearToWork[year] = [];
-    }
-    yearToWork[year].push(work);
-  });
-
-  const buttons = Object.keys(yearToWork)
-    .sort((a, b) => +new Date(b) - +new Date(a))
-    .reduce<IButton[]>((acc, year) => {
-      return [
-        ...acc,
-        {
-          label: year,
-          active: false,
-        },
-        ...yearToWork[year].map((work) => ({
-          label: work.name,
-          active: work === activeWork,
-          callback: () => {
-            callback(work);
-          },
-        })),
-      ];
-    }, []);
+export default function Sidebar({ works, callback, activeWork }: PropTypes) {
+  const buttons = works.map((work) => ({
+    label: work.name,
+    active: work === activeWork,
+    callback: () => {
+      callback(work);
+    },
+  }));
 
   return (
     <div className="sidebar">
-      <FunctionMenu
-        orientation={'vertical'}
-        buttons={buttons}
-      />
+      <FunctionMenu orientation={'vertical'} buttons={buttons} />
     </div>
   );
 }
